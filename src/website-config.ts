7f44a5fc34682b7e85a9bce790723565bf4562d8
amzbox <67238000+amzbox@users.noreply.github.com>
2020-06-22 01:50:14 +0700

    Update website-config.ts

--- src/website-config.ts
+++ src/website-config.ts
@@ -61,7 +61,7 @@ const config: WebsiteConfig = {
   mailchimpAction: 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
   mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
   mailchimpEmailFieldName: 'MERGE0',
-  googleSiteVerification: 'GoogleCode',
+  googleSiteVerification: 'cyNZ3wWOr9SdzJb61lC_rE2E2PJvsTB1qt6uL9j1xSk',
   footer: 'is based on Gatsby Casper',
 };
 
