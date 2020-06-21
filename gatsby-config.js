4ed8a770801f7c5d6dffe5c17da75659b7dae628
amzbox <67238000+amzbox@users.noreply.github.com>
2020-06-22 01:59:52 +0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,7 +72,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: 'UA-XXXX-Y',
+        trackingId: 'UA-163161690-8',
         // Puts tracking script in the head instead of the body
         head: true,
         // IP anonymization for GDPR compliance
